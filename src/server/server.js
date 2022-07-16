65481df6f2167faca169eb19fcd269dfcb435d7a
Ajay Kumar Gupta <ajaygupta25297@gmail.com>
2022-07-16 19:32:31 +0530

    final

--- src/server/server.js
+++ src/server/server.js
@@ -311,40 +311,9 @@ router.get("/insert", (req, res) => {
 
 });
 
-// router.post("/updateData", (req, res) => {
-//   const { id, update } = req.body;
-//   Data.findByIdAndUpdate(id, update, err => {
-//     if (err) return res.json({ success: false, error: err });
-//     return res.json({ success: true });
-//   });
-// });
-
-// router.delete("/deleteData", (req, res) => {
-//   const { id } = req.body;
-//   Data.findByIdAndRemove(id, err => {
-//     if (err) return res.send(err);
-//     return res.json({ success: true });
-//   });
-// });
-
-// router.post("/putData", (req, res) => {
-//   let data = new Data();
-
-//   const { id, message } = req.body;
-
-//   if ((!id && id !== 0) || !message) {
-//     return res.json({
-//       success: false,
-//       error: "INVALID INPUTS"
-//     });
-//   }
-//   data.message = message;
-//   data.id = id;
-//   data.save(err => {
-//     if (err) return res.json({ success: false, error: err });
-//     return res.json({ success: true });
-//   });
-// });
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static("server/build"))
+}
 
 app.use("/api", router);
 
